d63e36160b827c4066eaa4f8e0ae7f7ab96fe639
ravin <ravin@LAPTOP-QGI293UT>
2026-05-07 13:08:57 +0530

    cha;nges for test cases

---
+++ cat-facts.Tests/Controllers/CatFactsControllerTests.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using cat_facts.Controllers;
+using cat_facts.Model;
+using cat_facts.Service;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace cat_facts.Tests.Controllers
+{
+    public class CatFactsControllerTests
+    {
+        private readonly Mock<ICatFactService> _mockService;
+        private readonly CatFactsController _controller;
+
+        public CatFactsControllerTests()
+        {
+            _mockService = new Mock<ICatFactService>();
+            _controller = new CatFactsController(_mockService.Object);
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnOkResult_WithFacts()
+        {
+            // Arrange
+            var facts = new List<CatFact>
+            {
+                new CatFact
+                {
+                    Id = 1,
+                    Fact = "Cats sleep 16 hours"
+                },
+                new CatFact
+                {
+                    Id = 2,
+                    Fact = "Cats have whiskers"
+                }
+            };
+
+            _mockService.Setup(s => s.GetAll()).Returns(facts);
+
+            // Act
+            var result = _controller.GetAll();
+
+            // Assert
+            var okResult = result as OkObjectResult;
+
+            okResult.Should().NotBeNull();
+            okResult!.StatusCode.Should().Be(200);
+
+            var returnedFacts = okResult.Value as List<CatFact>;
+
+            returnedFacts.Should().NotBeNull();
+            returnedFacts.Should().BeEquivalentTo(facts);
+        }
+
+        [Fact]
+        public void GetRandom_ShouldReturnOk_WhenFactExists()
+        {
+            // Arrange
+            var fact = new CatFact
+            {
+                Id = 1,
+                Fact = "Cats can jump high"
+            };
+
+            _mockService.Setup(s => s.GetRandom()).Returns(fact);
+
+            // Act
+            var result = _controller.GetRandom();
+
+            // Assert
+            var okResult = result as OkObjectResult;
+
+            okResult.Should().NotBeNull();
+            okResult!.StatusCode.Should().Be(200);
+
+            var returnedFact = okResult.Value as CatFact;
+
+            returnedFact.Should().NotBeNull();
+            returnedFact.Should().BeEquivalentTo(fact);
+        }
+
+        [Fact]
+        public void GetRandom_ShouldReturnNotFound_WhenNoFactExists()
+        {
+            // Arrange
+            _mockService.Setup(s => s.GetRandom()).Returns((CatFact?)null);
+
+            // Act
+            var result = _controller.GetRandom();
+
+            // Assert
+            var notFoundResult = result as NotFoundObjectResult;
+
+            notFoundResult.Should().NotBeNull();
+            notFoundResult!.StatusCode.Should().Be(404);
+            notFoundResult.Value.Should().Be("No facts available");
+        }
+
+        [Fact]
+        public void Add_ShouldReturnBadRequest_WhenFactIsEmpty()
+        {
+            // Arrange
+            string fact = "";
+
+            // Act
+            var result = _controller.Add(fact);
+
+            // Assert
+            var badRequestResult = result as BadRequestObjectResult;
+
+            badRequestResult.Should().NotBeNull();
+            badRequestResult!.StatusCode.Should().Be(400);
+            badRequestResult.Value.Should().Be("Fact cannot be empty");
+        }
+
+        [Fact]
+        public void Add_ShouldReturnOk_WhenFactIsValid()
+        {
+            // Arrange
+            string fact = "Cats purr";
+
+            var addedFact = new CatFact
+            {
+                Id = 1,
+                Fact = fact
+            };
+
+            _mockService.Setup(s => s.Add(fact)).Returns(addedFact);
+
+            // Act
+            var result = _controller.Add(fact);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+
+            okResult.Should().NotBeNull();
+            okResult!.StatusCode.Should().Be(200);
+
+            var returnedFact = okResult.Value as CatFact;
+
+            returnedFact.Should().NotBeNull();
+            returnedFact!.Fact.Should().Be(fact);
+            returnedFact.Id.Should().Be(1);
+        }
+    }
+}
---
+++ cat-facts.Tests/Service/CatFactServiceTests.cs
@@ -0,0 +1,113 @@
+﻿using cat_facts.Model;
+using cat_facts.Service;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using System.IO;
+using Xunit;
+
+namespace cat_facts.Tests.Services
+{
+    [Collection("Sequential")]
+
+    public class CatFactServiceTests
+    {
+        private readonly CatFactService _service;
+        private readonly string _filePath;
+        private readonly Mock<IWebHostEnvironment> _mockEnv;
+
+        public CatFactServiceTests()
+        {
+            // ✔ Get project root (cat_facts project)
+            var basePath = Directory.GetCurrentDirectory();
+            var projectRoot = Path.GetFullPath(Path.Combine(basePath, @"..\..\.."));
+
+            // ✔ Point to real Data folder in API project
+            _filePath = Path.Combine(projectRoot, "Data", "catfact.json");
+
+            // ✔ Ensure folder exists
+            var dir = Path.GetDirectoryName(_filePath);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir!);
+            }
+
+            // ✔ RESET FILE before each test (VERY IMPORTANT)
+            File.WriteAllText(_filePath, "[]");
+
+            // ✔ Mock environment to point to project root
+            _mockEnv = new Mock<IWebHostEnvironment>();
+            _mockEnv.Setup(x => x.ContentRootPath).Returns(projectRoot);
+
+            // ✔ Create service
+            _service = new CatFactService(_mockEnv.Object);
+        }
+
+        [Fact]
+        public void Add_ShouldAddFact_AndReturnIt()
+        {
+            // Act
+            var result = _service.Add("Cats sleep a lot");
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Fact.Should().Be("Cats sleep a lot");
+            result.Id.Should().Be(1);
+
+            var all = _service.GetAll();
+            all.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnFacts()
+        {
+            // Arrange
+            _service.Add("Fact 1");
+            _service.Add("Fact 2");
+
+            // Act
+            var result = _service.GetAll();
+
+            // Assert
+            result.Should().HaveCount(2);
+            result.Should().Contain(x => x.Fact == "Fact 1");
+            result.Should().Contain(x => x.Fact == "Fact 2");
+        }
+
+        [Fact]
+        public void GetRandom_ShouldReturnNull_WhenEmpty()
+        {
+            // Act
+            var result = _service.GetRandom();
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetRandom_ShouldReturnFact_WhenExists()
+        {
+            // Arrange
+            _service.Add("Fact 1");
+
+            // Act
+            var result = _service.GetRandom();
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Fact.Should().Be("Fact 1");
+        }
+
+        [Fact]
+        public void Add_ShouldIncrementIdCorrectly()
+        {
+            // Act
+            var first = _service.Add("First");
+            var second = _service.Add("Second");
+
+            // Assert
+            first.Id.Should().Be(1);
+            second.Id.Should().Be(2);
+        }
+    }
+}
